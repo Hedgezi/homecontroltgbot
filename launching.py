495f15d673fd46de4942014149fb0ead0c1b2db0
Hedgezi <nikola@rootnix.net>
2022-11-21 00:57:41 +0100

    fixed the missing lib error, by just deleting it

--- launching.py
+++ launching.py
@@ -1,5 +1,5 @@
 import asyncio
-import datetime, zoneinfo
+import datetime
 
 from cleaning import *
 from trash import *
@@ -7,8 +7,7 @@ from trash import *
 
 async def main():
     global curthrasher, bot
-    # myscheduler.add_job(sendscheduleofroutines, 'interval', days=7, id='sendschedules', start_date=datetime.datetime(2022, 11, 21, 21, tzinfo=zoneinfo.ZoneInfo("Europe/Prague")))
-    myscheduler.add_job(sendscheduleofroutines, 'interval', seconds=10, id='sendschedules', start_date=datetime.datetime(2022, 11, 21, 0, 21, tzinfo=zoneinfo.ZoneInfo("Europe/Prague")))
+    myscheduler.add_job(sendscheduleofroutines, 'interval', days=7, id='sendschedules', start_date=datetime.datetime(2022, 11, 21, 21))
     myscheduler.start()
     await dp.start_polling(bot)
     
