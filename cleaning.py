3a1567b0cb20faeec6e8702f6f0f3a4a46e2a84e
Hedgezi <nikola@rootnix.net>
2022-11-21 01:05:54 +0100

    added command /currentcleaning to show current schedule of cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,5 @@
+from aiogram.filters import Command
+from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import json
 
@@ -15,3 +17,10 @@ async def sendscheduleofroutines():
         await bot.send_message(user[1], f'ты делаешь: <b>{work}</b>', parse_mode='HTML')
     with open("states.json", 'w') as f:
         json.dump({'curthr': curthrasher, 'coffset': cleaningoffset}, f)
+
+@dp.message(Command(commands=['currentcleaning']))
+async def curcleaningcmd(message: Message):
+    schstr = ''
+    for user, work in zip(users, [i for i in cleaningwork[cleaningoffset:len(cleaningwork)] + cleaningwork[0:cleaningoffset]]):
+        schstr += f'{user[0]}: {work}\n'
+    await message.answer(schstr, parse_mode='HTML')
