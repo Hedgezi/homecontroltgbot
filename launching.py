09b16900ef3bf9e438929c8d0a1c5ee0f266b3ff
Hedgezi <nikola@rootnix.net>
2023-01-03 23:39:34 +0100

    init commit

--- launching.py
+++ launching.py
@@ -3,10 +3,12 @@ import datetime
 
 from cleaning import *
 from trash import *
+from moneyreport import *
 
 
 async def main():
     global curthrasher, bot
+    dp.include_router(form_router)
     myscheduler.add_job(sendscheduleofroutines, 'interval', days=7, id='sendschedules', start_date=datetime.datetime(2022, 11, 21, 21))
     myscheduler.start()
     await dp.start_polling(bot)
