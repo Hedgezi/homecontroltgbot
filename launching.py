dee1d4c464d597329a9cae5680249baaf9ec5d13
Hedgezi <nikola@rootnix.net>
2023-03-06 16:23:08 +0100

    updated preferences ^^

--- launching.py
+++ launching.py
@@ -9,7 +9,8 @@ from moneyreport import *
 async def main():
     global curthrasher, bot
     dp.include_router(form_router)
-    myscheduler.add_job(sendscheduleofroutines, 'interval', days=7, id='sendschedules', start_date=datetime.datetime(2022, 11, 21, 21))
+    st_date = datetime.datetime(2023, 3, 13, 21)
+    myscheduler.add_job(sendscheduleofroutines, 'interval', days=14, id='sendschedules', start_date=st_date)
     myscheduler.start()
     await dp.start_polling(bot)
     
